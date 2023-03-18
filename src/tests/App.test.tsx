3703b92fcee2f9d68262a4f9836466586a5c566e
Alexander Martinovich <alexander_martinovich@msn.com>
2023-03-18 22:33:11 +0300

    fix: add router wrap to basic test

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,18 +1,22 @@
 import { describe, it } from 'vitest';
 import { render, screen } from '@testing-library/react';
-
 import App from '../App';
+import { BrowserRouter } from 'react-router-dom';
 
 describe('App', () => {
   it('Renders h1', () => {
     // ARRANGE
-    render(<App />);
+    render(
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    );
     // ACT
     // EXPECT
     expect(
       screen.getByRole('heading', {
         level: 1,
       })
-    ).toHaveTextContent('Vite + React');
+    ).toHaveTextContent('home page');
   });
 });
