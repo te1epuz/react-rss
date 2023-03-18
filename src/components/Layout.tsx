1d32fcef67ad1fee78fce16516d97fa326f375b6
Alexander Martinovich <alexander_martinovich@msn.com>
2023-03-19 02:09:58 +0300

    refactor: change to class components according to the task

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,21 +1,24 @@
+import React from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
-function Layout() {
-  return (
-    <>
-      <header>
-        <h2>this is header</h2>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </nav>
-      </header>
-      <Outlet />
-      <footer>
-        <h3>this is footer</h3>
-      </footer>
-    </>
-  );
+class Layout extends React.Component {
+  render() {
+    return (
+      <>
+        <header>
+          <h2>this is header</h2>
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+          </nav>
+        </header>
+        <Outlet />
+        <footer>
+          <h3>this is footer</h3>
+        </footer>
+      </>
+    );
+  }
 }
 
 export default Layout;
