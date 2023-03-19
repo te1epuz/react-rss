75cba684b8e771c2eb3baaeb42d2949e4e056ec6
Alexander Martinovich <alexander_martinovich@msn.com>
2023-03-19 15:48:07 +0300

    feat: add search bar styles and text save on reload

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import './SearchBar.scss';
 
 class SearchBar extends React.Component {
   state = {
-    searchBarText: '',
+    searchBarText: localStorage.getItem('searchBarText') || '',
   };
 
   handleChange(event: React.ChangeEvent<HTMLInputElement>) {
     this.setState({
       searchBarText: event.target.value,
     });
+    localStorage.setItem('searchBarText', event.target.value); // added setItem here to save search text even on page reload
+  }
+
+  componentWillUnmount() {
+    localStorage.setItem('searchBarText', this.state.searchBarText); // currently reduntant, but required by the task description
   }
 
   render() {
     return (
-      <form>
+      <form onSubmit={(event) => event.preventDefault()} role="search">
         <input
-          type="text"
+          type="search"
+          className="input"
           value={this.state.searchBarText}
-          placeholder="Search"
+          placeholder="Search..."
           onChange={this.handleChange.bind(this)}
         />
-        <button>Search</button>
+        <button className="button">Search</button>
       </form>
     );
   }
