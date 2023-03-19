b06850a0671db32a2e013f07e0b596ad45a533ce
Alexander Martinovich <alexander_martinovich@msn.com>
2023-03-19 19:20:42 +0300

    feat: add search result list and cards

---
+++ src/components/ResultsCard.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { TProduct } from '../services/types';
+import './ResultsCard.scss';
+
+type TProps = {
+  result: TProduct;
+};
+
+class ResultsCard extends React.Component<TProps> {
+  render() {
+    const { result } = this.props;
+    return (
+      <div className="card card__content_grid">
+        <div className="card__content">
+          <div className="img__wrapper">
+            <img className="img" src={result.thumbnail} alt={result.title} />
+          </div>
+          <div className="price">
+            <h3>${result.price}</h3>
+            <p className="text_rating">Rating: {result.rating}</p>
+          </div>
+          <p className="title">{result.title}</p>
+          <div className="disabled">
+            <p className="text_greyed">Categoty: {result.category}</p>
+            <p className="text_greyed">Brand: {result.brand}</p>
+            <p className="text_greyed">
+              <span>Discount: {result.discountPercentage}% | </span>
+              <span>Stock: {result.stock}</span>
+            </p>
+          </div>
+        </div>
+
+        <div className="card__buttons">
+          <button className="card__button" type="button">
+            Details
+          </button>
+
+          <button className="card__button" type="button">
+            2nd button
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ResultsCard;
---
+++ src/services/types.ts
@@ -0,0 +1,17 @@
+export type TProduct = {
+  id: number | string;
+  brand: string;
+  category: string;
+  description: string;
+  discountPercentage: number;
+  images: string[];
+  price: number;
+  rating: number;
+  stock: number;
+  thumbnail: string;
+  title: string;
+};
+
+export type TResponse = {
+  products: TProduct[];
+};
