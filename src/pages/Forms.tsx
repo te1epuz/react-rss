14a197c70378960a60b4c34748f32f239423aa3b
Alexander Martinovich <alexander_martinovich@msn.com>
2023-03-25 16:28:37 +0300

    feat: add form submit and card create basic functionality

--- src/pages/Forms.tsx
+++ src/pages/Forms.tsx
@@ -1,60 +1,96 @@
 import React, { RefObject } from 'react';
 import { TFormData } from '../services/types';
 
+type TFormCard = {
+  id: number;
+  name: string;
+  gender: 'male' | 'female';
+  date: string;
+  country: string;
+  checkbox1: boolean;
+  checkbox2: boolean;
+  file: string;
+};
+
+type TState = {
+  formData: TFormData;
+  cardsList: TFormCard[];
+};
+
 class Forms extends React.Component {
-  formData: TFormData = {
-    name: React.createRef(),
-    radioMale: React.createRef(),
-    radioFemale: React.createRef(),
-    date: React.createRef(),
-    country: React.createRef(),
-    checkbox1: React.createRef(),
-    checkbox2: React.createRef(),
-    file: React.createRef(),
+  state: TState = {
+    formData: {
+      name: React.createRef(),
+      radioMale: React.createRef(),
+      radioFemale: React.createRef(),
+      date: React.createRef(),
+      country: React.createRef(),
+      checkbox1: React.createRef(),
+      checkbox2: React.createRef(),
+      file: React.createRef(),
+    },
+    cardsList: [],
   };
 
   inputName: RefObject<HTMLInputElement> = React.createRef();
 
   handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log('name:', this.formData.name.current?.value);
-    console.log('male:', this.formData.radioMale.current?.checked);
-    console.log('female:', this.formData.radioFemale.current?.checked);
-    console.log('date:', this.formData.date.current?.value);
-    console.log('country:', this.formData.country.current?.value);
-    console.log('checkbox1:', this.formData.checkbox1.current?.checked);
-    console.log('checkbox2:', this.formData.checkbox2.current?.checked);
-    console.log('file:', this.formData.file.current?.value);
+    console.log('name:', this.state.formData.name.current?.value);
+    console.log('male:', this.state.formData.radioMale.current?.checked);
+    console.log('female:', this.state.formData.radioFemale.current?.checked);
+    console.log('date:', this.state.formData.date.current?.value);
+    console.log('country:', this.state.formData.country.current?.value);
+    console.log('checkbox1:', this.state.formData.checkbox1.current?.checked);
+    console.log('checkbox2:', this.state.formData.checkbox2.current?.checked);
+    console.log('file:', this.state.formData.file.current?.value);
+    this.setState({
+      cardsList: [
+        ...this.state.cardsList,
+        {
+          id: this.state.cardsList.length + 1,
+          name: this.state.formData.name.current?.value || 'no name',
+          gender: this.state.formData.radioMale.current?.checked ? 'male' : 'female',
+          date: this.state.formData.date.current?.value,
+          country: this.state.formData.country.current?.value,
+          checkbox1: this.state.formData.checkbox1.current?.checked,
+          checkbox2: this.state.formData.checkbox2.current?.checked,
+          file: this.state.formData.file.current?.value,
+        },
+      ],
+    });
+    event.currentTarget.reset();
   }
 
   render() {
     return (
       <main>
         <h1>Forms page</h1>
+        <hr />
         <form onSubmit={this.handleSubmit.bind(this)}>
           <label>
-            Name: <br />
-            <input type="text" ref={this.formData.name} />
+            Name:&nbsp;
+            <input type="text" ref={this.state.formData.name} />
           </label>
           <br />
           <label>
-            <input name="gender" type="radio" ref={this.formData.radioMale} />
+            <input name="gender" type="radio" ref={this.state.formData.radioMale} />
             male <br />
           </label>
           <br />
           <label>
-            <input name="gender" type="radio" ref={this.formData.radioFemale} />
+            <input name="gender" type="radio" ref={this.state.formData.radioFemale} />
             female <br />
           </label>
           <br />
           <label>
-            Date of birth: <br />
-            <input type="date" ref={this.formData.date} />
+            Date of birth:&nbsp;
+            <input type="date" ref={this.state.formData.date} />
           </label>
           <br />
           <label>
-            Country: <br />
-            <select defaultValue="none" ref={this.formData.country}>
+            Country:&nbsp;
+            <select defaultValue="none" ref={this.state.formData.country}>
               <option value="none" disabled>
                 Select country
               </option>
@@ -65,22 +101,37 @@ class Forms extends React.Component {
           </label>
           <br />
           <label>
-            <input type="checkbox" ref={this.formData.checkbox1} />
+            <input type="checkbox" ref={this.state.formData.checkbox1} />
             Share all my personal date <br />
           </label>
           <br />
           <label>
-            <input type="checkbox" ref={this.formData.checkbox2} />
+            <input type="checkbox" ref={this.state.formData.checkbox2} />
             Share my current location <br />
           </label>
           <br />
           <label>
-            Upload file:
-            <input type="file" ref={this.formData.file} />
+            Upload file:&nbsp;
+            <input type="file" ref={this.state.formData.file} />
           </label>
           <br />
           <button>Submit</button>
         </form>
+        <div>
+          <hr />
+          <h2>Cards list</h2>
+          <div>
+            {this.state.cardsList.map((card) => (
+              <div key={card.id}>
+                <h3>{card.name}</h3>
+                <p>{card.gender}</p>
+                <p>{card.date}</p>
+                <p>{card.country}</p>
+                <p>{card.file}</p>
+              </div>
+            ))}
+          </div>
+        </div>
       </main>
     );
   }
