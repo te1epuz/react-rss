828a6509414f19d22e10604736ccad0219a48c38
Alexander Martinovich <alexander_martinovich@msn.com>
2023-04-10 14:30:27 +0700

    fix: fix lint errors

--- src/tests/Home.test.tsx
+++ src/tests/Home.test.tsx
@@ -2,41 +2,6 @@ import { describe, it } from 'vitest';
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react';
 import Home from '../pages/Home';
 import { BrowserRouter } from 'react-router-dom';
-import SearchResults from '../components/Home/SearchResults';
-
-const searchResults = {
-  info: {
-    count: 29,
-    pages: 2,
-    next: 'https://rickandmortyapi.com/api/character/?page=2&name=rick&status=alive',
-    prev: null,
-  },
-  results: [
-    {
-      id: 1,
-      name: 'Rick Sanchez',
-      status: 'Alive',
-      species: 'Human',
-      type: '',
-      gender: 'Male',
-      origin: {
-        name: 'Earth',
-        url: 'https://rickandmortyapi.com/api/location/1',
-      },
-      location: {
-        name: 'Earth',
-        url: 'https://rickandmortyapi.com/api/location/20',
-      },
-      image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
-      episode: [
-        'https://rickandmortyapi.com/api/episode/1',
-        'https://rickandmortyapi.com/api/episode/2',
-      ],
-      url: 'https://rickandmortyapi.com/api/character/1',
-      created: '2017-11-04T18:48:46.250Z',
-    },
-  ],
-};
 
 describe('Home', () => {
   it('Renders h1', () => {
